fd65deaa89de7b5a36fe84a9f7a49c5392f76a75
MyDrugs <111056393+Gerafftes@users.noreply.github.com>
2022-08-14 22:36:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,82 +4,52 @@ const listContainer = document.getElementsByClassName("list-container")[0]
 toggleButton.addEventListener("click", () =>{
     listContainer.classList.toggle("active")
 })
+
 const wrapper = document.querySelector(".sliderWrapper");
 const menuItems = document.querySelectorAll(".menuItem");
 
 const products = [
   {
     id: 1,
-    title: "Air Force",
+    title: "Green Snowflake",
     price: 119,
     colors: [
       {
         code: "black",
-        img: "./img/air.png",
-      },
-      {
-        code: "darkblue",
-        img: "./img/air2.png",
+        img: "Bilder/unnamed.png",
       },
     ],
   },
   {
     id: 2,
-    title: "Air Jordan",
+    title: "Purple Flower",
     price: 149,
     colors: [
       {
         code: "lightgray",
-        img: "./img/jordan.png",
-      },
-      {
-        code: "green",
-        img: "./img/jordan2.png",
+        img: "Bilder/unnamed3.png/",
       },
     ],
   },
   {
     id: 3,
-    title: "Blazer",
+    title: "Red Pill",
     price: 109,
     colors: [
       {
         code: "lightgray",
-        img: "./img/blazer.png",
-      },
-      {
-        code: "green",
-        img: "./img/blazer2.png",
+        img: "Bilder/unnamed4.png",
       },
     ],
   },
   {
     id: 4,
-    title: "Crater",
+    title: "Blue Clover",
     price: 129,
     colors: [
       {
         code: "black",
-        img: "./img/crater.png",
-      },
-      {
-        code: "lightgray",
-        img: "./img/crater2.png",
-      },
-    ],
-  },
-  {
-    id: 5,
-    title: "Hippie",
-    price: 99,
-    colors: [
-      {
-        code: "gray",
-        img: "./img/hippie.png",
-      },
-      {
-        code: "black",
-        img: "./img/hippie2.png",
+        img: "Bilder/unnamed2.png",
       },
     ],
   },
